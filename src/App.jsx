4f037bd1dafa28ac368d6efd235082cb615a621d
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-12 10:01:38 -0700

    Header jsx and css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
+//import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const fetchSongs = async () => {
+    // try {
+    //   const response = await axios.get("https://localhost:7197/api/Songs");
+    //   console.log(response);
+    // } catch (error) {
+    //   console.warn("Error in fetch Songs request", error);
+    // }
+  };
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
