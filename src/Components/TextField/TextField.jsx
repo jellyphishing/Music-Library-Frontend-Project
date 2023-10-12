727b8c6280e13e2f853f72ea3596a7b585f83ba6
Lorraine Martin <lolocodes2001@gmail.com>
2023-10-12 11:43:32 -0700

    built TextField component

---
+++ src/Components/TextField/TextField.jsx
@@ -0,0 +1,11 @@
+import "./TextField.css";
+
+const TextField = ({ label, value, onChange }) => {
+  return (
+    <div className="text-field">
+      <label>{label}</label>
+      <input value={value} onChange={(e) => onChange(e.target.value)} />
+    </div>
+  );
+};
+export default TextField;
